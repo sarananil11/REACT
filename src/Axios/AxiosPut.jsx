22275997c05e4c1eaca0249a32cb7080bc08c4c6
sarananil11 <sarananilvp2020@gmail.com>
2025-11-18 10:01:45 +0530

    refactor: clean up formatting and improve readability in AxiosPut component

--- src/Axios/AxiosPut.jsx
+++ src/Axios/AxiosPut.jsx
@@ -1,26 +1,25 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-// import { resolveConfig } from 'vite';
 
 function AxiosPut() {
-    const [message,setMessage]= useState("");
+  const [message, setMessage] = useState("");
 
-    const updateUser = async () => {
-        try {
-            const response = await axios.put(
-                "https://jsonplaceholder.typicode.com/users/1",
-                {
-                    name:"Updated User",
-                    email:"updated@example.com"
-                }
-            )
-            setMessage("User updated succesfully!")
-            console.log(response.data)
-        } catch (error) {
-            console.error("PUT error",error);
-            
+  const updateUser = async () => {
+    try {
+      const response = await axios.put(
+        "https://jsonplaceholder.typicode.com/users/1",
+        {
+          name: "Updated User",
+          email: "updated@example.com"
         }
+      )
+      setMessage("User updated succesfully!")
+      console.log(response.data)
+    } catch (error) {
+      console.error("PUT error", error);
+
     }
+  }
   return (
     <div>
       <h2>Axios PUT example</h2>
